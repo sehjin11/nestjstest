33c2fab4b02d3dfaa039c0647ea4ca08b6b8b4a1
sehjin <sehjin11@gmail.com>
2022-01-17 23:16:38 +0900

    auth 진행중

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 
 @Module({
+  imports: [
+    PassportModule.register({ defaultStrategy: 'jwt' }),
+    JwtModule.register({
+      secret: 'Secret1234',
+      signOptions: {
+        expiresIn: 60 * 60,
+      },
+    }),
+  ],
   controllers: [AuthController],
-  providers: [AuthService]
+  providers: [AuthService],
 })
 export class AuthModule {}
--- src/configs/typeorm.config.ts
+++ src/configs/typeorm.config.ts
@@ -5,7 +5,7 @@ export const typeORMconfig: TypeOrmModuleOptions = {
   host: 'localhost',
   port: 5432,
   username: 'postgres',
-  password: '1234',
+  password: '3336',
   database: 'board-app',
   entities: [__dirname + '/../**/*.entity.{js,ts}'],
   synchronize: true,
